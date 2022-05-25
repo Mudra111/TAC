ea5255e34179d7e81abc29275e9d9900479b761c
Jahnavi <jahnavipankajpatel@gmail.com>
2022-05-25 14:43:05 +0530

    Login pae of TAC

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import './App.css';
+import "./App.css";
 import Header from "../src/Header";
 import Home from "../src/Home";
 import Footer from "./componentes/Footer";
 import Insta from "./componentes/Insta";
-import { 
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <Header/>
+      <Header />
       <Routes>
-      <Route exact path="/" element={<Home/>}/>
-      <Route exact path="/insta" element={<Insta/>}/>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/insta" element={<Insta />} />
       </Routes>
-      <Footer/>
-      </Router>
+      <Footer />
+    </Router>
   );
 }
 
